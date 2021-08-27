029612bc719ea4e669138867e254fc54ed338742
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-27 15:35:58 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/custom_card_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.custom_card_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MainPage();
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Custom Card Example',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Color(0xFF8C062F),
+      ),
+    );
+  }
+}
