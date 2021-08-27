7c7d29e2b8416512f3dab99d568df84a2f8ecdc5
Yusril Ihsanul Alim <73714271+Rillwz@users.noreply.github.com>
2021-08-27 15:51:30 +0700

    custom card example

--- lib/main.dart
+++ lib/main.dart
@@ -18,13 +18,35 @@ class MainPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Custom Card Example',
-          style: TextStyle(color: Colors.white),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Custom Card Example',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Color(0xFF8C062F),
         ),
-        backgroundColor: Color(0xFF8C062F),
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Color(0xFFFE5788), Color(0xFFF56D5D)],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+            ),
+            Center(
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.8,
+                height: MediaQuery.of(context).size.height * 0.7,
+                child: Card(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
